a2e4a1f69b65ae879e419bfb58fbedcdd8f99e0d
MiketheBite <migueljcpinto@mail.com>
2024-03-01 00:18:28 +0100

    styles corrections

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,17 +17,17 @@ const Home = async ({ searchParams }: SearchParamProps) => {
       <section className="sm:flex-center hidden h-95 relative w-full overflow-hidden bg-white items-center justify-center  h-72 flex-col gap-4 rounded-[20px] border bg-cover bg-no-repeat p-10">
         <div className="absolute inset-0 w-full h-full bg-slate-100 z-20 [mask-image:linear-gradient(transparent,white)] pointer-events-none" />
         <Boxes />
-        <h1 className={cn("md:text-4xl text-xl text-dark-600 relative z-20")}>
-          Unleash Your Creative Vision with LensAI
+        <h1 className={cn("md:text-4xl text-xl text-dark-800 relative z-20")}>
+          Unleash Your Creative Vision
         </h1>
-        <ul className="flex-center w-full gap-20 text-center mt-10 text-neutral-300 relative z-20">
+        {/*         <ul className="flex-center w-full gap-20 text-center mt-10 text-neutral-300 relative z-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
               key={link.route}
               href={link.route}
-              className="flex-center flex-col gap-2"
+              className="flex-center flex-col gap-2 "
             >
-              <li className="flex-center w-fit rounded-full bg-white p-4 border">
+              <li className="flex-center w-fit rounded-full bg-white/60 p-4 border hover:bg-yellow-100/40">
                 <Image src={link.icon} alt="image" width={24} height={24} />
               </li>
               <p className="p-14-medium text-center text-dark-600">
@@ -36,6 +36,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
             </Link>
           ))}
         </ul>
+ */}{" "}
       </section>
 
       <section className="sm:mt-12">
