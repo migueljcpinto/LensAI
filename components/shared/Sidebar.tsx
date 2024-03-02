d23281b391b699219959a7752a918357b716f4b1
MiketheBite <migueljcpinto@mail.com>
2024-03-02 15:33:38 +0100

    banner name

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -15,12 +15,12 @@ const Sidebar = () => {
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
-          {/* <Image
+          <Image
             src="/assets/images/logo-text.svg"
             alt="logo"
             width={180}
             height={28}
-          /> */}
+          />
         </Link>
 
         <nav className="sidebar-nav">
